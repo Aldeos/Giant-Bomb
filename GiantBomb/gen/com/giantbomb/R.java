d69d8e6b5da49925fa0a194c75020e15561f6d09
Nick <Nick@NickCs>
2014-04-16 21:11:06 -0400

    2nd commit

--- GiantBomb/gen/com/giantbomb/R.java
+++ GiantBomb/gen/com/giantbomb/R.java
@@ -21,8 +21,24 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
+        public static final int ButtonEnvoyer=0x7f080002;
+        public static final int EditTextSearch=0x7f080001;
+        public static final int action_settings=0x7f080011;
+        public static final int author_name=0x7f08000e;
+        public static final int date=0x7f080007;
+        public static final int description=0x7f080005;
+        public static final int during=0x7f08000b;
+        public static final int genre=0x7f08000a;
+        public static final int img=0x7f080003;
+        public static final int number_of_user_review=0x7f080008;
         public static final int pager=0x7f080000;
+        public static final int plateform=0x7f080006;
+        public static final int publish_date_movie=0x7f08000c;
+        public static final int publish_date_review=0x7f08000f;
+        public static final int ratingBar1=0x7f080010;
+        public static final int title_movie=0x7f080009;
+        public static final int title_review=0x7f08000d;
+        public static final int titre=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -36,7 +52,12 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int button=0x7f050007;
+        public static final int games_icon=0x7f050003;
         public static final int hello_world=0x7f050002;
+        public static final int movies_icon=0x7f050004;
+        public static final int reviews_icon=0x7f050005;
+        public static final int search=0x7f050006;
     }
     public static final class style {
         /** 
